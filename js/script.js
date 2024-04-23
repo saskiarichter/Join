1a2a4f9f000ff39fb0e300bfc8912667ae0f061a
Onur Copur <copuronur@outlook.comm>
2024-04-23 17:49:12 +0200

    added form sign up

---
+++ js/script.js
@@ -0,0 +1,39 @@
+function changeForm() {
+    document.getElementById('signUpContainer').innerHTML = "";
+    let form = document.getElementById('form');
+    form.classList.add("formSignUp");
+    let checkboxSignUp = document.getElementById('checkboxSignUp');
+    checkboxSignUp.classList.add("loginCheckboxSignUp");
+
+    form.innerHTML = "";
+
+    form.innerHTML = `
+                        <div class="formHeadline">
+                            <h1 class="formHeadlineText">Sign up</h1>
+                            <div class="formHeadlineBorder"></div>
+                        </div>    
+                        <div class="mailPassword">
+                            <div>
+                                <input class="inputNameSignUp" type="text" aria-describedby="emailHelp" placeholder="Name" required>
+                            </div>    
+                        <div>
+                            <input class="inputMailSignUp" type="email" aria-describedby="emailHelp" placeholder="Email" required>
+                        </div>
+                        <div>
+                            <input class="inputPasswordSignUp" type="password"  placeholder="Password" required>
+                        </div>
+                        <div>
+                            <input class="inputPasswordSignUp" type="password" placeholder="Confirm Password" required>
+                        </div>
+                        
+                        <div class="notification"></div>  
+                        <div class="loginCheckboxSignUp">
+                            <input type="checkbox" class="loginCheckBoxRememberMe" id="loginCheckBoxRememberMe">
+                            <label for="loginCheckBoxRememberMe" class="loginCheckBoxRememberMeLabel"></label>
+                            <p for="exampleCheck1">I accept the <a class="policeLink" href"#">Privacy Police</a></label>
+                        </div>
+                        <div class="buttons">
+                            <button class="buttonLogin" type="submit">Sign up</button>
+                        </div> 
+    `;
+}
