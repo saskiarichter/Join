9d4ef3339fcda90b5786f335ad39be7e541bd85a
Onur Copur <copuronur@outlook.comm>
2024-04-25 13:33:33 +0200

    added legalNotice.html

--- js/script.js
+++ js/script.js
@@ -61,23 +61,6 @@ function changeForm() {
 }
 
 
-function changeBg(clickedElement) {
-    // Alle Links holen
-    let links = document.querySelectorAll('.links, .linksNav');
-
-    // Für jeden Link prüfen, ob er angeklickt wurde und die Klasse entsprechend hinzufügen/entfernen
-    Array.from(links).forEach(link => {
-        if (link === clickedElement.parentElement || link === clickedElement) {
-            // Dem angeklickten Link die Klasse hinzufügen
-            link.classList.add('bgfocus');
-        } else {
-            // Allen anderen Links die Klasse entfernen
-            link.classList.remove('bgfocus');
-        }
-    });
-}
-
-
 function returnToHome() {
     window.location.href = "index.html";
 }
@@ -85,6 +68,10 @@ function returnToHome() {
 function redirectToHelp() {
     window.location.href = "help.html";
 }
+
+
+
+
 
 
 
