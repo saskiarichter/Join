f7b30396937afa81b2ba4d715c1c7e2f679fd669
saskiarichter <richter_saskia@gmx.net>
2024-04-27 13:58:23 +0200

    addTask update

--- js/addTask.js
+++ js/addTask.js
@@ -1,4 +1,46 @@
-function init(){
+function init() {
     initInclude();
 }
 
+function titleRequired() {
+    document.getElementById('addTask-title').classList.remove('empty');
+    document.getElementById('required-title').classList.add('transparent');
+}
+
+function dueDateRequired() {
+    document.getElementById('addTask-dueDate').classList.remove('empty');
+    document.getElementById('required-date').classList.add('transparent');
+}
+
+function categoryRequired() {
+    document.getElementById('addTask-category').classList.remove('empty');
+}
+
+function checkInput() {
+    let ids = ['addTask-title', 'addTask-dueDate', 'addTask-category'];
+    for (let i = 0; i < ids.length; i++) {
+        const id = ids[i];
+        let element = document.getElementById(`${id}`);
+        if (element.value === '') {
+            element.classList.add('empty');
+            document.getElementById('required-title').classList.remove('transparent');
+            document.getElementById('required-date').classList.remove('transparent');
+        }else{
+            createTask();
+            emptyInput();
+        }
+    }
+}
+
+function emptyInput(){
+    document.getElementById('addTask-title').value = '';
+    document.getElementById('addTask-description').value = '';
+    document.getElementById('addTask-assigned').value = '';
+    document.getElementById('addTask-dueDate').value = '';
+    document.getElementById('addTask-category').value = '';
+    document.getElementById('addTask-subtasks').value = '';
+}
+
+function createTask(){
+
+}
