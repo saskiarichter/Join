9725a4fd7df17ec361e9f6851054c903427fa417
saskiarichter <richter_saskia@gmx.net>
2024-04-29 12:12:48 +0200

    addTask buttons & hover effects

--- js/addTask.js
+++ js/addTask.js
@@ -1,26 +1,50 @@
+/**
+ * 
+ * loads navBar and header
+ */
 async function init() {
     await initInclude();
     addTaskBgMenu();
 }
 
+/**
+ * 
+ * Adds bgcolor on current Page in the NavBar 
+ */
 function addTaskBgMenu() {
     document.getElementById('addTaskMenu').classList.add('bgfocus');
 }
 
+/**
+ * 
+ * Removes red color from Title input 
+ */
 function titleRequired() {
     document.getElementById('addTask-title').classList.remove('empty');
     document.getElementById('required-title').classList.add('transparent');
 }
 
+/**
+ * 
+ * Removes red color from DueDate input
+ */
 function dueDateRequired() {
     document.getElementById('addTask-dueDate').classList.remove('empty');
     document.getElementById('required-date').classList.add('transparent');
 }
 
+/**
+ * 
+ * Removes red color from Category
+ */
 function categoryRequired() {
     document.getElementById('addTask-category').classList.remove('empty');
 }
 
+/**
+ * 
+ * checks if required inputs are filled out 
+ */
 function checkInput() {
     let ids = ['addTask-title', 'addTask-dueDate', 'addTask-category'];
     for (let i = 0; i < ids.length; i++) {
@@ -30,14 +54,18 @@ function checkInput() {
             element.classList.add('empty');
             document.getElementById('required-title').classList.remove('transparent');
             document.getElementById('required-date').classList.remove('transparent');
-        }else{
+        } else {
             createTask();
             emptyInput();
         }
     }
 }
 
-function emptyInput(){
+/**
+ * 
+ * empties all inputfields 
+ */
+function emptyInput() {
     document.getElementById('addTask-title').value = '';
     document.getElementById('addTask-description').value = '';
     document.getElementById('addTask-assigned').value = '';
@@ -46,6 +74,6 @@ function emptyInput(){
     document.getElementById('addTask-subtasks').value = '';
 }
 
-function createTask(){
+function createTask() {
 
 }
