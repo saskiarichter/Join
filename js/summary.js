0fbfca1507cf19513025661b67c6441ce88f5747
Onur Copur <copuronur@outlook.comm>
2024-04-29 17:19:13 +0200

    update summary.html/js

--- js/summary.js
+++ js/summary.js
@@ -1,6 +1,7 @@
 async function initSummary() {
     await includeHTML();
     summaryBg();
+    updateGreeting();
 }
 
 function summaryBg() {
@@ -8,4 +9,18 @@ function summaryBg() {
 }
 
 
+    // Funktion zur Bestimmung der Tageszeit und Aktualisierung der Begrüßung
+    function updateGreeting() {
+        let currentTime = new Date();
+        let currentHour = currentTime.getHours();
 
+        if (currentHour < 12) {
+            greeting = "Good morning,";
+        } else if (currentHour < 18) {
+            greeting = "Good afternoon,";
+        } else {
+            greeting = "Good evening,";
+        }
+
+        document.getElementById("daytimeGreeting").innerText = greeting;
+    }
